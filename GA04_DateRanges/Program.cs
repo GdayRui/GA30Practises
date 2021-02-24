235383c639e52f04defdfc32586a95eea8551006
Rui <lirui.jobs@gmail.com>
2021-02-24 13:29:31 +1100

    code refactor

--- GA04_DateRanges/Program.cs
+++ GA04_DateRanges/Program.cs
@@ -15,8 +15,6 @@ namespace GA04_DateRanges
         {
             Console.WriteLine("Hello World!");
 
-            //string[] givenDates = { "2016-07-31", "2020-09-21" };
-
             MakeFriendlyDates(new string[] { "2016-07-31", "2020-09-21" });
             MakeFriendlyDates(new string[] { "2016-07-31", "2016-09-21" });
             MakeFriendlyDates(new string[] { "2016-07-01", "2016-07-31" });
@@ -93,9 +91,6 @@ namespace GA04_DateRanges
         {
             string[] friendlyDates = new string[2];
 
-            //string beginningFriendlyDate = "";
-            //string endFrindlyDate = "";
-
             string friendlyYearOfBeginning = "";
             string friendlyMonthOfBeginning = "";
             string friendlyDayOfBeginning = "";
@@ -106,20 +101,21 @@ namespace GA04_DateRanges
 
             string[] months = { "January", "Feburay", "March", "Apirl", "May", "June", "July", "August", "September", "October", "November", "December" };
 
+
             // 1. Separate the given dates to year, month and date, then convert them to int
+
             DateTime localDate = DateTime.Now;
             string thisYear = localDate.Year.ToString();
 
             string[] beginningDate = givenDates[0].Split("-");
             string[] endDate = givenDates[1].Split("-");
 
+
             // 2. If the beginning date's year is the curren year, convert the beginning date without displaying the year
 
             // beginning year
             if (beginningDate[0] != thisYear)
-            {
-                friendlyYearOfBeginning = ", " + beginningDate[0];
-            }
+                friendlyYearOfBeginning = ", " + beginningDate[0]; 
 
             // beginning month
             int month = int.Parse(beginningDate[1]);
@@ -128,34 +124,16 @@ namespace GA04_DateRanges
             // beginning day
             int day = int.Parse(beginningDate[2]);
             friendlyDayOfBeginning = ConvertDay(day, friendlyDayOfBeginning);
-            //switch (day)
-            //{
-            //    case 1:
-            //    case 21:
-            //    case 31:
-            //        friendlyDayOfBeginning = day.ToString() + "st";
-            //        break;
-            //    case 2:
-            //    case 22:
-            //        friendlyDayOfBeginning = day.ToString() + "nd";
-            //        break;
-            //    case 3:
-            //    case 23:
-            //        friendlyDayOfBeginning = day.ToString() + "rd";
-            //        break;
-            //    default:
-            //        friendlyDayOfBeginning = day.ToString() + "th";
-            //        break;
-            //}
-
+           
             string beginningFriendlyDate = friendlyMonthOfBeginning + friendlyDayOfBeginning + friendlyYearOfBeginning;
+
 
             // 3. If the end year is same as beginning year, convert the end date without displaying the year; month following
             //    otherwise, convert them all
+
             if (endDate[0] != beginningDate[0])
-            {
                 friendlyYearOfEnd = ", " + endDate[0];
-            }
+
             // end month
             int monthOfEnd = int.Parse(endDate[1]);
             if (endDate[1] != beginningDate[1])
@@ -164,26 +142,7 @@ namespace GA04_DateRanges
             // end day
             int dayOfEnd = int.Parse(endDate[2]);
             friendlyDayOfEnd = ConvertDay(dayOfEnd, friendlyDayOfEnd);
-            //switch (dayOfEnd)
-            //{
-            //    case 1:
-            //    case 21:
-            //    case 31:
-            //        friendlyDayOfEnd = dayOfEnd.ToString() + "st";
-            //        break;
-            //    case 2:
-            //    case 22:
-            //        friendlyDayOfEnd = dayOfEnd.ToString() + "nd";
-            //        break;
-            //    case 3:
-            //    case 23:
-            //        friendlyDayOfEnd = dayOfEnd.ToString() + "rd";
-            //        break;
-            //    default:
-            //        friendlyDayOfEnd = dayOfEnd.ToString() + "th";
-            //        break;
-            //}
-
+            
             string endFrindlyDate = friendlyMonthOfEnd + friendlyDayOfEnd + friendlyYearOfEnd;
 
             friendlyDates[0] = beginningFriendlyDate;
