8a3886cca0264c9fb9999ea7befb25184466c667
Rui <lirui.jobs@gmail.com>
2021-03-12 13:31:22 +1100

    DCA code test 1

---
+++ DCAtest01_RotateString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DCAtest01_RotateString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
