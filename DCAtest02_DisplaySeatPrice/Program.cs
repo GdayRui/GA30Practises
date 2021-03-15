f198cb926560f3094d9c6ffe73956647b80b8161
Rui <lirui.jobs@gmail.com>
2021-03-15 12:53:07 +1100

    DCA code test 2

---
+++ DCAtest02_DisplaySeatPrice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DCAtest02_DisplaySeatPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DisplaySeatPrice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DisplaySeatPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
